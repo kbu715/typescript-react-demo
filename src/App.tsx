04594d9bde5d08231c670dda92b27faf7368c9da
kbu715 <kbu715@gmail.com>
2020-10-17 20:44:27 +0900

    typescript+react study

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { createGlobalStyle } from "styled-components";
+import { Form, Input } from './Input';
+import Number from './Number';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+interface IState {
+  counter : number;
+  name: string;
+}
+
+class App extends Component<{}, IState> {
+  state = {
+    counter : 0,
+    name: ""
+  };
+  render() {
+    const { counter, name } = this.state;
+    return (
+    <div>
+      <Form onFormSubmit={this.onFormSubmit}>
+        <Input value={name} onChange={this.onChange} />
+      </Form>
+      <Number count={counter} /> <button onClick={this.add}>Add</button>
     </div>
-  );
+    );
+  }
+
+  onChange = (event:React.SyntheticEvent<HTMLInputElement>) => {
+    console.log(event)
+  }
+
+
+  onFormSubmit = (event:React.FormEvent) => {
+    event.preventDefault();
+  }
+
+
+  add = ():void => {
+    this.setState(prev => {
+      return {
+        counter : prev.counter + 1,
+      }
+    })
+  }
 }
 
 export default App;
---
+++ src/Input.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+
+interface IInputProps {
+    value: string;
+    onChange: (event:React.SyntheticEvent<HTMLInputElement>) => void;
+}
+
+export const Input : React.FunctionComponent<IInputProps> = ({value, onChange}) => (
+    <input type="text" placeholder="Name" value={value} onChange={onChange} />  
+)
+
+
+
+
+interface IFormProps {
+    onFormSubmit: (event:React.FormEvent) => void;
+}
+
+export const Form : React.FunctionComponent<IFormProps> = ({children, onFormSubmit}) => (
+    <form onSubmit={onFormSubmit}>{children}</form>
+)
